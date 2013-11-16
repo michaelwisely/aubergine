7312d3d17e8fde03c84efdae76cd248bfb19d491
Michael Wisely <michaelwisely@gmail.com>
2013-11-16 13:09:23 -0600

    Update celery settings
    There were problems with Celery loading the default Django settings
    instead of development.py or production.py, and it was upsetting
    Django. This way, django and celery things are more isolated.

--- aubergine/celery.py
+++ aubergine/celery.py
@@ -1,26 +1,23 @@
 from __future__ import absolute_import
 
-import os
-
 from celery import Celery
-from django.conf import settings
 
-
-# Set up Aubergine development app
-os.environ['DJANGO_SETTINGS_MODULE'] = 'aubergine.settings.defaults'
 
 app = Celery('aubergine')
 app.config_from_object('aubergine.settings.celeryconfig')
-app.autodiscover_tasks(settings.INSTALLED_APPS, related_name='tasks')
+app.autodiscover_tasks(['aubergine'], related_name='tasks')
 app.setup_security()
+
 
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
+
 @app.task()
 def tadd(x, y):
     return x + y
+
 
 @app.task()
 def tsum(numbers):
--- aubergine/settings/celeryconfig.py
+++ aubergine/settings/celeryconfig.py
@@ -1,10 +1,90 @@
+import os
+import sys
+
+SETTINGS_DIR = os.path.dirname(__file__)
+PROJECT_DIR = os.path.dirname(SETTINGS_DIR)
+BUILDOUT_DIR = os.path.dirname(PROJECT_DIR)
+VAR_DIR = os.path.join(BUILDOUT_DIR, "var")
+
+
+##########################################################################
+#
+# Secret settings
+#
+##########################################################################
+
+# If a secret_settings file isn't defined, open a new one and save a
+# SECRET_KEY in it. Then import it. All passwords and other secret
+# settings should be stored in secret_settings.py. NOT in settings.py
+try:
+
+    from secret_settings import CELERY_SECURITY_KEY
+    from secret_settings import CELERY_SECURITY_CERTIFICATE
+    from secret_settings import CELERY_SECURITY_CERT_STORE
+except ImportError:
+    sys.stderr.write("""
+        Couldn't load CELERY_SECURITY_* settings from secret_settings.py.
+        Check to be sure that the following settings are defined:
+
+            CELERY_SECURITY_KEY
+            CELERY_SECURITY_CERTIFICATE
+            CELERY_SECURITY_CERT_STORE
+
+        This may require generating some SSL keys and certificates.\n\n""")
+    sys.exit(1)
+
+
+##########################################################################
+#
+# Broker settings
+#
+##########################################################################
+
 BROKER_URL = 'mongodb://127.0.0.1/celery'
+
+
+##########################################################################
+#
+# Results settings
+#
+##########################################################################
 
 CELERY_RESULT_BACKEND = 'mongodb://127.0.0.1/celery_results'
 CELERY_RESULT_SERIALIZER = 'json'
+
+
+##########################################################################
+#
+# Task settings
+#
+##########################################################################
+
 CELERY_TASK_SERIALIZER = 'auth'
+
+
+##########################################################################
+#
+# Worker settings
+#
+##########################################################################
+
 CELERY_ACCEPT_CONTENT = ['json', 'auth']
+
+
+##########################################################################
+#
+# Beat settings
+#
+##########################################################################
+
 CELERYBEAT_SCHEDULE_FILENAME = 'var/db/celery-schedule.db'
+
+
+##########################################################################
+#
+# Security settings
+#
+##########################################################################
 
 # Should be set in secret_settings.py
 # CELERY_SECURITY_KEY = ""
